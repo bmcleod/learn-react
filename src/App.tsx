146558e500b87eb1106711736c3b42dbcc2ab4a3
Lincoln Anderson <landerson@threefivetwo.com>
2022-04-10 20:43:34 -0400

    add privacy warning

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 
 import AuthProvider from './auth/AuthProvider';
+import AuthButton from './auth/AuthButton';
+import PrivacyWarning from './components/PrivacyWarning';
 import ExamplesPage from './pages/ExamplesPage';
 import CompositionalPatterns from './pages/CompositionalPatterns';
 import JavascriptPatterns from './pages/JavascriptPatterns';
 import PlopperPage from './pages/PlopperPage';
-import AuthButton from './auth/AuthButton';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
+      <PrivacyWarning />
       <AuthButton />
       <BrowserRouter>
         <QueryParamProvider ReactRouterRoute={Route}>
---
+++ src/components/PrivacyWarning.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as UI from '@chakra-ui/react';
+
+const PrivacyWarning: React.FC = () => {
+  return (
+    <UI.Alert colorScheme="orange" justifyContent="center">
+      <UI.Text textAlign="center">
+        This is an early prototype app with very loose privacy rules. Please do
+        not paste any secure or private information.
+      </UI.Text>
+    </UI.Alert>
+  );
+};
+
+export default PrivacyWarning;
